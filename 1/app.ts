63fa73ac2b95a9032962f8b01f8b757989305162
Hannes Schaletzky <hannes.schaletzky@digitalservice.bund.de>
2022-12-01 16:51:24 +0100

    1 done

--- 1/app.ts
+++ 1/app.ts
@@ -1,1 +1,20 @@
-console.log('test')
+import fs from 'fs'
+
+const sums: number[] = []
+let sum = 0
+
+fs.readFileSync(`${__dirname}/input.txt`, 'utf-8')
+  .split(/\r?\n/)
+  .forEach((line: string) => {
+    if (line.length == 0) {
+      sums.push(sum)
+      sum = 0
+    } else {
+      sum += Number(line)
+    }
+  })
+
+sums.sort((a: number, b: number) => b - a)
+console.log(sums)
+console.log(sums[0])
+console.log(sums[0] + sums[1] + sums[2])
