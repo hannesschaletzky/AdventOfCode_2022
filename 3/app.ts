5ab143baf3ed4d4d2f14522ea9eb80d0dec628a7
Hannes Schaletzky <hannes.schaletzky@gmx.de>
2022-12-03 13:05:00 +0100

    3 done

--- 3/app.ts
+++ 3/app.ts
@@ -1,5 +1,46 @@
-// solutions:
+// solutions: https://www.reddit.com/r/adventofcode/comments/zb865p/2022_day_3_solutions/
 import { getLinesOfFile } from 'utils/fs'
 
-const lines = getLinesOfFile(`${__dirname}/input.txt`)
-console.log(lines.length)
+const lines = getLinesOfFile()
+
+let lowerAlphabet = [...Array(26).keys()].map((i) =>
+  String.fromCharCode(i + 97)
+)
+let capitalAlphabet = [...Array(26).keys()].map((i) =>
+  String.fromCharCode(i + 65)
+)
+let joinedAlphabet = lowerAlphabet.concat(capitalAlphabet)
+
+// part 1
+let sum1 = 0
+lines.forEach((line) => {
+  const middle = line.length / 2
+  const first = [...line.substring(0, middle)]
+  const second = line.substring(middle)
+
+  first.every((c) => {
+    if (second.includes(c)) {
+      sum1 += joinedAlphabet.indexOf(c) + 1
+      return false
+    }
+    return true
+  })
+})
+console.log(sum1)
+
+// part 2
+let sum2 = 0
+for (let i = 0; i < lines.length; i += 3) {
+  const first = [...lines[i]]
+  const second = lines[i + 1]
+  const third = lines[i + 2]
+
+  first.every((c) => {
+    if (second.includes(c) && third.includes(c)) {
+      sum2 += joinedAlphabet.indexOf(c) + 1
+      return false
+    }
+    return true
+  })
+}
+console.log(sum2)
--- utils/fs.ts
+++ utils/fs.ts
@@ -1,5 +1,15 @@
 import fs from 'fs'
 
-export const getLinesOfFile = (filePath: string): string[] => {
-  return fs.readFileSync(filePath, 'utf-8').split(/\r?\n/)
+export const getLinesOfFile = (): string[] => {
+  // determine which input file to use based on process.argv[2]
+  let pathFragments = process.argv[1].split('/')
+  pathFragments.pop()
+  if (process.argv[2] == 'test') {
+    pathFragments.push('input_test.txt')
+  } else {
+    pathFragments.push('input.txt')
+  }
+  const path = pathFragments.join('/')
+  console.log(path)
+  return fs.readFileSync(path, 'utf-8').split(/\r?\n/)
 }
