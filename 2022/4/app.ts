574d056c3337e3a3ba76838e4cbcd9bdcd55dd55
Hannes Schaletzky <hannes.schaletzky@gmx.de>
2022-12-04 13:29:06 +0100

    4 init

---
+++ 2022/4/app.ts
@@ -0,0 +1,12 @@
+// solutions: https://www.reddit.com/r/adventofcode/comments/r8i1lq/2021_day_4_solutions/
+import { getInputLines } from 'utils/fs'
+
+const lines = getInputLines()
+
+// part 1
+let sum1 = 0
+console.log(sum1)
+
+// part 2
+let sum2 = 0
+console.log(sum2)
