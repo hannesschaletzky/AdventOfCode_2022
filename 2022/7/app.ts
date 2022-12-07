057cc9afc06bb8ffa40a00c6d138aa9c073094df
Hannes Schaletzky <hannes.schaletzky@gmx.de>
2022-12-08 00:09:43 +0100

    7 init

---
+++ 2022/7/app.ts
@@ -0,0 +1,13 @@
+import 'utils/extensions'
+import { getInputLines, getInputFile } from 'utils/fs'
+
+const lines = getInputLines()
+const file = getInputFile()
+
+// part 1
+let ans1
+console.log(ans1)
+
+// part 2
+let ans2 = 0
+console.log(ans2)
