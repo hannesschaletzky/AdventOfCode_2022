2bcc140e2c53dc7896684e6c13a548d451590ef1
Hannes Schaletzky <hannes.schaletzky@digitalservice.bund.de>
2022-12-07 16:56:18 +0100

    refactored setup.ts

--- 2022/utils/setup.ts
+++ 2022/utils/setup.ts
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import fs from 'fs'
+import readline from 'readline'
 import { JSDOM } from 'jsdom'
 import * as dotenv from 'dotenv'
 dotenv.config()
@@ -9,6 +10,7 @@ const cookie = process.env.AOC_SESSION_COOKIE
 const _basePath = './_base'
 const inputName = 'input.txt'
 const inputTestName = 'input_test.txt'
+const taskName = 'task.md'
 
 // read year and day from argv
 if (isNaN(+process.argv[2]) || isNaN(+process.argv[3])) {
@@ -22,56 +24,101 @@ console.log('setting up: ', year, day)
 
 const newPath = `./${day}`
 
-console.log(newPath)
+console.log(
+  `----------------------------\n create directory ${newPath}\n----------------------------`
+)
 
 if (fs.existsSync(newPath)) {
-  console.log('removing dir')
+  console.log('removing old')
   fs.readdirSync(newPath).forEach((file) => {
     fs.rmSync(`${newPath}/${file}`)
   })
   fs.rmdirSync(newPath)
 }
-console.log('creating dir')
+console.log('creating new')
 fs.mkdirSync(newPath)
 
 fs.readdirSync(_basePath).forEach((file) => {
+  console.log('copy file: ', file)
   fs.cpSync(`${_basePath}/${file}`, `${newPath}/${file}`)
 })
 
-// scrape content and save into files
-
 // https://adventofcode.com/2022/day/7/input
-const inputURL = `${baseURI}/${year}/day/${day}/input`
 const taskURL = `${baseURI}/${year}/day/${day}`
+const inputURL = `${taskURL}/input`
+scrapeAndSaveFiles()
 
-axios
-  .get(inputURL, {
-    headers: {
-      'Content-Type': 'text/plain',
-      Cookie: `session=${cookie}`
+async function scrapeAndSaveFiles() {
+  console.log(
+    `----------------------------\nfetching & saving files\n----------------------------`
+  )
+  // input
+  console.log('from: ', inputURL)
+  const inputData = await getResponseData(inputURL)
+  const inputPath = `${newPath}/${inputName}`
+  fs.writeFileSync(inputPath, inputData)
+  console.log('saved to: ', inputPath)
+
+  // task
+  console.log('\nfrom: ', taskURL)
+  const taskData = await getResponseData(taskURL)
+  const document = new JSDOM(taskData).window.document
+  const taskText = document.querySelector('article')?.textContent
+  const taskPath = `${newPath}/${taskName}`
+  fs.writeFileSync(taskPath, taskText!)
+  console.log('saved to: ', taskPath)
+
+  // example / test input
+  const inputTestPath = `${newPath}/${inputTestName}`
+  const blocks = document.querySelectorAll('pre')
+  console.log('----------------------------\nchoose correct test input')
+
+  for (let i = 0; i < blocks.length; i++) {
+    const block = blocks[i]
+    const answer = await askBlock(block)
+    if (answer == 'yes') {
+      fs.writeFileSync(inputTestPath, block.textContent!)
+      console.log('saved to: ', inputTestPath)
+      break
     }
-  })
-  .then((res) => {
-    console.log('received data with length: ', res.data.length)
-    fs.writeFileSync(`${newPath}/${inputName}`, res.data)
-  })
-  .catch((err) => {
-    console.log(err)
-  })
+  }
 
-axios
-  .get(taskURL, {
-    headers: {
-      'Content-Type': 'text/plain',
-      Cookie: `session=${cookie}`
-    }
-  })
-  .then((res) => {
-    const document = new JSDOM(res.data).window.document
-    console.log(document.querySelector('code'))
-  })
-  .catch((err) => {
-    console.log(err)
-  })
+  console.log('\n\n\n🥳 setup done!\n')
+  process.exit(0)
+}
 
-// const options: OptionsOfTextResponseBody =
+function askBlock(block: HTMLPreElement) {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  })
+  return new Promise((resolve) => {
+    console.log('----------------------------')
+    console.log(block.previousElementSibling?.textContent, '\n')
+    console.log(block.textContent)
+    rl.question('-> Type "yes" ✅ or "no" ❌\n', (ans: string) => {
+      rl.close()
+      resolve(ans)
+    })
+  })
+}
+
+function getResponseData(url: string): Promise<string> {
+  return new Promise<string>((resolve, reject) => {
+    axios
+      .get(url, {
+        headers: {
+          'Content-Type': 'text/plain',
+          Cookie: `session=${cookie}`
+        }
+      })
+      .then((res) => {
+        console.log('received data')
+        resolve(res.data)
+      })
+      .catch((err) => {
+        console.log(err)
+        reject(err)
+      })
+  })
+}
