51e91c7d32394e1221e1051a0acab4236db9d202
Hannes Schaletzky <hannes.schaletzky@gmx.de>
2022-12-11 18:14:00 +0100

    10 only last number wrong

--- 2022/10/app.ts
+++ 2022/10/app.ts
@@ -2,59 +2,103 @@ import 'utils/extensions'
 import { lines } from 'utils/fs'
 
 interface Task {
-  cycles: number
+  finishedAtCycle: number
   calculation: number
 }
 let queue: Task[] = []
 let x = 1
 let sum = 0
-let cycles = 0
-for (let i = 0; i < lines.length; i++) {
+let cycle = 0
+
+lines.forEach((line, i) => {
+  const split = line.split(' ')
+  const operation = split[0]
+  const number = Number(split[1])
+
+  console.log(cycle, i, operation, number, 'x:', x)
+
+  if (operation == 'noop') {
+    checkSignalStrength()
+    cycle++
+  } else if (operation == 'addx') {
+    checkSignalStrength()
+    execCalculations()
+    const task: Task = {
+      finishedAtCycle: cycle + 1,
+      calculation: number
+    }
+    queue.push(task)
+    cycle++
+    checkSignalStrength()
+    execCalculations()
+    cycle++
+  }
+
+  function execCalculations() {
+    // exec calculation of finished tasks
+    queue.forEach((task) => {
+      if (task.finishedAtCycle == cycle) {
+        console.log('exec:', task.calculation)
+        x += task.calculation
+      }
+    })
+  }
+
+  function checkSignalStrength() {
+    // see signal strength
+    if (cycle == 20 || cycle % 40 == 20) {
+      const result = x * cycle
+      console.log('result:', `${x} * ${cycle} =`, result, '\n\n\n\n\n')
+      sum += result
+    }
+  }
+})
+
+// 1 + 15 - 11 + 6 - 3 + 5 - 1 - 8 + 13 + 4 = 21
+console.log(sum)
+
+for (let i = 1000000; i < lines.length; i++) {
   const line = lines[i]
   const split = line.split(' ')
   const operation = split[0]
   const number = Number(split[1])
-  cycles++
 
-  console.log(cycles, i, operation, number, 'x:', x)
+  console.log(cycle, i, operation, number, 'x:', x)
 
-  // see signal strength
-  if (cycles == 20 || cycles % 40 == 20) {
-    const result = x * cycles
-    console.log('result:', result, '\n\n\n\n\n')
-    sum += result
-  }
-
-  // add task to queue
-  if (operation == 'addx') {
-    cycles++
+  if (operation == 'noop') {
+    execCalculations()
+    checkSignalStrength()
+    cycle++
+  } else if (operation == 'addx') {
     const task: Task = {
-      cycles: 2,
+      finishedAtCycle: cycle + 2,
       calculation: number
     }
     queue.push(task)
+    cycle++
+    execCalculations()
+    checkSignalStrength()
   }
 
-  // reduce queued tasks by one cycle
-  queue = queue.map((task) => {
-    task.cycles--
-    return task
-  })
-
-  // extract finished tasks
-  const finishedTasks: Task[] = []
-  for (let j = queue.length - 1; j >= 0; j--) {
-    if (queue[j].cycles == 0) {
-      finishedTasks.push(...queue.splice(j, 1))
+  function checkSignalStrength() {
+    // see signal strength
+    if (cycle == 20 || cycle % 40 == 20) {
+      const result = x * cycle
+      console.log('result:', result, '\n\n\n\n\n')
+      sum += result
     }
   }
 
-  // include calculation of finished tasks
-  finishedTasks.forEach((task) => {
-    console.log('exec:', task.calculation)
-    x += task.calculation
-  })
+  function execCalculations() {
+    // exec calculation of finished tasks
+    queue.forEach((task) => {
+      if (task.finishedAtCycle == cycle) {
+        console.log('exec:', task.calculation)
+        x += task.calculation
+      }
+    })
+  }
 }
 
 // 1 + 15 - 11 + 6 - 3 + 5 - 1 - 8 + 13 + 4 = 21
-console.log(sum)
+// console.log(sum)
