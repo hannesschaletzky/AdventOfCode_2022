8d9522181823ad38b401b71022677308c4c76203
Hannes Schaletzky <hannes.schaletzky@gmx.de>
2022-12-12 20:59:18 +0100

    10 not done

--- 2022/10/app.ts
+++ 2022/10/app.ts
@@ -1,6 +1,8 @@
 import 'utils/extensions'
 import { lines } from 'utils/fs'
 
+// https://github.com/joeleisner/advent-of-code-2022/blob/main/days/10-cathode-ray-tube/mod.ts
+
 interface Task {
   finishedAtCycle: number
   calculation: number
@@ -49,55 +51,14 @@ lines.forEach((line, i) => {
       const result = x * cycle
       console.log('result:', `${x} * ${cycle} =`, result, '\n\n\n\n\n')
       sum += result
+      if (cycle == 220) {
+        console.log(sum)
+        process.exit(0)
+      }
     }
   }
 })
 
 // 1 + 15 - 11 + 6 - 3 + 5 - 1 - 8 + 13 + 4 = 21
-console.log(sum)
 
-for (let i = 1000000; i < lines.length; i++) {
-  const line = lines[i]
-  const split = line.split(' ')
-  const operation = split[0]
-  const number = Number(split[1])
-
-  console.log(cycle, i, operation, number, 'x:', x)
-
-  if (operation == 'noop') {
-    execCalculations()
-    checkSignalStrength()
-    cycle++
-  } else if (operation == 'addx') {
-    const task: Task = {
-      finishedAtCycle: cycle + 2,
-      calculation: number
-    }
-    queue.push(task)
-    cycle++
-    execCalculations()
-    checkSignalStrength()
-  }
-
-  function checkSignalStrength() {
-    // see signal strength
-    if (cycle == 20 || cycle % 40 == 20) {
-      const result = x * cycle
-      console.log('result:', result, '\n\n\n\n\n')
-      sum += result
-    }
-  }
-
-  function execCalculations() {
-    // exec calculation of finished tasks
-    queue.forEach((task) => {
-      if (task.finishedAtCycle == cycle) {
-        console.log('exec:', task.calculation)
-        x += task.calculation
-      }
-    })
-  }
-}
-
-// 1 + 15 - 11 + 6 - 3 + 5 - 1 - 8 + 13 + 4 = 21
-// console.log(sum)
+// 16380 -> wrong
