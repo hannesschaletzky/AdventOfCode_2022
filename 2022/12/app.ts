ca7bb17266e89897ac526ec6b0f7da16755cd4a3
Hannes Schaletzky <hannes.schaletzky@gmx.de>
2022-12-19 18:01:52 +0100

    12 init

---
+++ 2022/12/app.ts
@@ -0,0 +1,9 @@
+import 'utils/extensions'
+import { getInputLines, getInputFile } from 'utils/fs'
+
+const lines = getInputLines()
+const file = getInputFile()
+
+// part 1
+let sum = 0
+console.log('sum:', sum)
