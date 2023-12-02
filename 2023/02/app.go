cb5884577e940f79317aad4e78b0a80d861f6999
Hannes Schaletzky <hannes.schaletzky@gmx.de>
2023-12-02 16:54:48 +0100

    day 2 part 2 done

--- 2023/02/app.go
+++ 2023/02/app.go
@@ -10,8 +10,8 @@ import (
 	"strings"
 )
 
-func getInputFile(number int64) (*os.File, *bufio.Scanner, error) {
-	file, err := os.Open(fmt.Sprintf("./input%d.txt", number))
+func getInputFile() (*os.File, *bufio.Scanner, error) {
+	file, err := os.Open("./input.txt")
 	if err != nil {
 		return nil, nil, err
 	}
@@ -30,23 +30,58 @@ func exceedsBoundary(color string, line string, boundary int) bool {
 	for _, element := range matches {
 		num, err := strconv.Atoi(element)
 		if err != nil {
-			fmt.Println(err)
+			log.Fatal(err)
 		}
 		if num > boundary {
 			return true
 		}
 	}
-
 	return false
 }
 
+func maxValueOfColor(color string, line string) int {
+	re := regexp.MustCompile(fmt.Sprintf(`(\d+) %s`, color))
+	matches := re.FindAllString(line, -1)
+	for i, element := range matches {
+		matches[i] = strings.ReplaceAll(element, fmt.Sprintf(` %s`, color), "")
+	}
+	fmt.Println(color, matches)
+
+	maxValue := 0
+	for _, element := range matches {
+		num, err := strconv.Atoi(element)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if num > maxValue {
+			maxValue = num
+		}
+	}
+	return maxValue
+}
+
 func main() {
-	file, scanner, err := getInputFile(1)
+	file, scanner, err := getInputFile()
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer file.Close()
 
+	part1(*scanner)
+	part2(*scanner)
+}
+
+func part2(scanner bufio.Scanner) {
+	sum := 0
+	for scanner.Scan() {
+		line := scanner.Text()
+		fmt.Println(line)
+		sum += maxValueOfColor("red", line) * maxValueOfColor("green", line) * maxValueOfColor("blue", line)
+	}
+	fmt.Println(sum)
+}
+
+func part1(scanner bufio.Scanner) {
 	sum := 0
 	for scanner.Scan() {
 		line := scanner.Text()
@@ -56,7 +91,7 @@ func main() {
 		matches := re.FindAllString(line, -1)
 		id, err := strconv.Atoi(matches[0])
 		if err != nil {
-			fmt.Println(err)
+			log.Fatal(err)
 		}
 		fmt.Println("id", id)
 
