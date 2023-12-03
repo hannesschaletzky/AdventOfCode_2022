e0fa95158b32a05870c61dd476da790263cd0ba1
Hannes Schaletzky <hannes.schaletzky@gmx.de>
2023-12-04 00:01:46 +0100

    day 3 init

---
+++ 2023/03/app.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"2023/utils"
+	"fmt"
+	"log"
+)
+
+// &+-#$*@%
+
+func main() {
+	file, scanner, err := utils.GetInputFile()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		fmt.Println(line)
+	}
+}
